4805b1067f02340516f804583ac005a0aa2e2ac6
zhangzhikai <zzkbean@163.com>
2022-12-27 11:06:38 +0000

    人脸识别重构完成

--- face_detect.py
+++ face_detect.py
@@ -2,21 +2,33 @@
 Author: zhangzhikai zzkbean@163.com
 Date: 2022-12-27 09:52:58
 LastEditors: zhangzhikai zzkbean@163.com
-LastEditTime: 2022-12-27 09:57:14
+LastEditTime: 2022-12-27 11:00:31
 FilePath: /raspi-python/face_detect.py
 Description: [Edit]
 
 Copyright (c) 2022 by zhangzhikai zzkbean@163.com, All Rights Reserved. 
 '''
 import base64
+import os
 import time
 import urllib
-from http.client import IncompleteRead, RemoteDisconnected
+# from http.client import IncompleteRead, RemoteDisconnected
 from urllib.error import HTTPError, URLError  # URLError与HTTPError的异常处理
+
+import requests  # 百度语音sdk
+from aip import AipSpeech
+
+# 百度云api连接参数
+baidu_config = {
+    'APP_ID': '28833160',
+    'API_KEY': 'cqnAOIksYK6mFkwToYpRpjil',
+    'SECRET_KEY': '4ssnT8EZceyvDuDCXtxk0jpCzBS6O86w'
+}
+client = AipSpeech(
+        baidu_config['APP_ID'], baidu_config['API_KEY'], baidu_config['SECRET_KEY'])
 
 
 def verify(access_token):
-    baidu_tts("您好，进门前请进行身份认证")
     print("开始认证...")
     img = get_picture()
     faceSearch(img, access_token)
@@ -37,17 +49,27 @@ def get_picture(num_retries=3):
         print(e.reason)
     except URLError as e:  # 一定要放到HTTPError之后，因为它包含了前者
         print(e.reason)
-    except IncompleteRead or RemoteDisconnected as e:
-        if num_retries == 0:  # 重连机制
-            return
-        else:
-            requestImg(num_retries - 1)
+    # except IncompleteRead or RemoteDisconnected as e:
+    #     if num_retries == 0:  # 重连机制
+    #         return
+    #     else:
+    #         requestImg(num_retries - 1)
     # 打开图片并转码  rb以二进制格式打开一个文件用于只读
     f = open('people.jpg', 'rb')
     img = base64.b64encode(f.read())
     return img
 
+# 人脸识别接口请求
+def getsearchMessage(img, access_token):
+    request_url = "https://aip.baidubce.com/rest/2.0/face/v3/search"
+    params = {"image": img, "image_type": "BASE64", "group_id_list": "face_list", "quality_control": "LOW",
+              "liveness_control": "NORMAL"}
+    request_url = request_url + "?access_token=" + access_token
+    response = requests.post(request_url, data=params)
+    return response.json()
+
 # 人脸搜索返回处理
+err_count = 0
 def faceSearch(img, access_token):
     output = getsearchMessage(img, access_token)
     if output['error_msg'] == 'SUCCESS':
@@ -55,17 +77,32 @@ def faceSearch(img, access_token):
         print(user_list)
         score = user_list[0]['score']
         user = user_list[0]['user_id']
-        if user == 'zhangzhikai':
-            words = "认证成功,张智凯" + "相似度为百分之" + \
-                    str(round(score, 2)) + ",快来唤醒小派吧!"
-            baidu_tts(words)
 
+        baidu_tts("您好，进门前请进行身份认证")
+
+        if user == 'zhangzhikai':
+            words = "认证成功,张智凯" + "相似度百分之" + \
+                    str(round(score, 2)) + "...请进！"
+            baidu_tts(words)
     else:
         print(output['error_msg'])
-        baidu_tts("对不起,认证失败!")
+        global err_count
+        err_count += 1
+        baidu_tts("对不起,认证失败!,请重新刷脸")
         time.sleep(1) 
-        baidu_tts("当前处于测试模式，可以直接唤醒小派！")
+        if err_count < 3:
+            verify(access_token)
+        else:
+            baidu_tts("已报警！")
+        
 
+# 监控人脸认证
+def monitorSearch(img, access_token):
+    output = getsearchMessage(img, access_token)
+    if output['error_msg'] != 'SUCCESS':
+        return 1
+    else:
+        pass
 
 # 百度语音合成 test to speech
 def baidu_tts(words):
@@ -77,3 +114,7 @@ def baidu_tts(words):
         os.system('mplayer audio.mp3')
     else:
         print(result)
+
+if __name__ == '__main__':
+    access_token = "24.9750ed527e2ecf5dd4d7dd0a9a235c3d.2592000.1673324251.282335-28833160"
+    verify(access_token)
